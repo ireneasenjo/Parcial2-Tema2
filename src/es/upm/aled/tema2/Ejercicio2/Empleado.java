5dfa6086df8697c805c0ee3707a9a67dcf78040d
ireneasenjo <irene.asenjo@alumnos.upm.es>
2025-11-20 19:30:41 +0100

    Ejercicio 2

---
+++ src/es/upm/aled/tema2/Ejercicio2/Empleado.java
@@ -0,0 +1,50 @@
+package es.upm.aled.tema2.Ejercicio2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empleado {
+	private String nombre;
+	private double salario;
+	private List<Empleado> subordinados; // Lista de empleados a su cargo
+
+	public Empleado(String nombre, double salario) {
+		this.nombre = nombre;
+		this.salario = salario;
+		this.subordinados = new ArrayList<Empleado>();
+	}
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getSalario() {
+		return this.salario;
+	}
+
+	public List<Empleado> getSubordinados() {
+		return this.subordinados;
+	}
+
+	public void addSubordinado(Empleado subordinado) {
+		this.subordinados.add(subordinado);
+	}
+
+	public static double presupuestoEquipo(Empleado jefe) {
+		if (jefe == null)
+			return 0.0;
+
+		// coste incluye el salario del jefe
+		double total = jefe.getSalario();
+
+		// Paso recursivo: sumar presupuestos de los subordinados
+		if (jefe.getSubordinados() != null) {
+			for (Empleado sub : jefe.getSubordinados()) {
+				total += presupuestoEquipo(sub);
+			}
+		}
+
+		return total;
+	}
+}
